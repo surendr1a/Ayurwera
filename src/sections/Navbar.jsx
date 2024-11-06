9f3d2a4457a782c44ce5a1efca0631e764119511
Surendra Chourasiya <surendrachourasiya420@gmail.com>
2024-11-06 20:08:33 +0530

    changing the theme of navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,3 @@
-// Navbar.js
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaUserCircle, FaBars, FaTimes } from 'react-icons/fa';
@@ -11,7 +10,7 @@ export default function Navbar() {
     };
 
     return (
-        <header className="fixed top-0 left-0 z-50 w-full bg-gradient-to-r from-indigo-600 to-pink-600 p-1 shadow-lg transition duration-300 ease-in-out">
+        <header className="fixed top-0 left-0 z-50 w-full bg-white p-1 shadow-lg transition duration-300 ease-in-out">
             <nav className="flex justify-between items-center">
                 <div className="flex items-center">
                     <a href="/">
@@ -21,32 +20,32 @@ export default function Navbar() {
 
                 {/* Hamburger Icon for Mobile */}
                 <div className="md:hidden mr-4">
-                    <button onClick={toggleMenu} className="text-white focus:outline-none">
+                    <button onClick={toggleMenu} className="text-green-600 focus:outline-none">
                         {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
                     </button>
                 </div>
 
                 {/* Menu Items */}
-                <ul className={`flex-col md:flex-row md:flex ${isOpen ? 'flex' : 'hidden'} md:space-x-8 text-lg font-semibold text-white md:mr-8`}>
+                <ul className={`flex-col md:flex-row md:flex ${isOpen ? 'flex' : 'hidden'} md:space-x-8 text-lg font-semibold text-green-600 md:mr-8`}>
                     <li className="relative group">
-                        <Link to="/" className="hover:text-gray-200 transition-colors duration-300">Home</Link>
-                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-pink-300 scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                        <Link to="/" className="text-green-600 hover:text-[#2D6A4F] transition-colors duration-300">Home</Link>
+                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-[#2D6A4F] scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
                     </li>
                     <li className="relative group">
-                        <Link to="/blog" className="hover:text-gray-200 transition-colors duration-300">Blog</Link>
-                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-pink-300 scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                        <Link to="/blog" className="text-green-600 hover:text-[#2D6A4F] transition-colors duration-300">Blog</Link>
+                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-[#2D6A4F] scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
                     </li>
                     <li className="relative group">
-                        <Link to="/login" className="hover:text-gray-200 transition-colors duration-300">
+                        <Link to="/login" className="text-green-600 hover:text-[#2D6A4F] transition-colors duration-300">
                             <FaUserCircle className="inline mr-1" /> Login
                         </Link>
-                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-pink-300 scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-[#2D6A4F] scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
                     </li>
                     <li className="relative group">
-                        <Link to="/signup" className="hover:text-gray-200 transition-colors duration-300">
+                        <Link to="/signup" className="text-green-600 hover:text-[#2D6A4F] transition-colors duration-300">
                             <FaUserCircle className="inline mr-1" /> Signup
                         </Link>
-                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-pink-300 scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                        <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-[#2D6A4F] scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
                     </li>
                 </ul>
             </nav>
