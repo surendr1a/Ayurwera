6820fb0ab7148f02407ee9df7f186bd0fc7b5253
Surendra Chourasiya <surendrachourasiya420@gmail.com>
2024-11-27 09:27:06 +0530

    just improving the products on home section and also adding the animations in them

--- src/sections/Sidebar.jsx
+++ src/sections/Sidebar.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { PiShoppingCartSimpleDuotone } from "react-icons/pi";
-import { AiOutlineLogin } from "react-icons/ai";
 import {
    FcHome,
    FcShop,
