20b6aac86e563f4d3ccb18ed5ee7247cb523ff56
Surendra Chourasiya <surendrachourasiya420@gmail.com>
2024-12-11 21:06:29 +0530

    removed the sidebar

--- src/pages/Consults/Consultants.jsx
+++ src/pages/Consults/Consultants.jsx
@@ -1,22 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { topConsultants } from "../../constants/index"; // Import your consultants data
 import ConsultsCard from "./ConsultsCard"; // The updated ProductCard component
-import Sidebar from "../../sections/Sidebar";
 
 const ConsultsList = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true); // Track sidebar state
-
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <Sidebar onSidebarToggle={setIsSidebarOpen} />
-
+    <div>
       {/* Cards Container */}
-      <div
-  className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-40 p-4 transition-all duration-300 ${
-    isSidebarOpen ? "lg:grid-cols-4  ml-64" : "lg:grid-cols-5 ml-20"
-  }`}
->
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-40 p-4">
         {topConsultants.map((consultant) => (
           <ConsultsCard key={consultant.id} consultant={consultant} />
         ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,12 @@ import Carousel from '../sections/Hero';
 import TopConsultants from '../sections/ConsultantsHome';
 import ReviewSection from '../sections/Review';
 import Navbar from '../sections/Navbar';
-import Sidebar from '../sections/Sidebar';
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <Sidebar/>
+   
       <Carousel />
       <TopConsultants/>
       <ReviewSection />
