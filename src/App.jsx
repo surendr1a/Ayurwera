03f2b9de6530881204987ad2728d6cac97a7c752
Surendra Chourasiya <surendrachourasiya420@gmail.com>
2024-12-21 11:42:46 +0530

    casual changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
 import Home from './pages/Home';
 import ProfileSection from './pages/Profile';
-import TopDoctors from './pages/Consults/Consultants';
+import TopDoctors from './pages/Doctors_page';
 
 function App() {
   return (
